b3bfd8bd895578a0a983af37227368e673ce09fb
almonick <almonick72@gmail.com>
2015-02-27 15:14:07 +0200

    add

---
+++ app/src/main/java/firstapp/studiolearn/com/firstapp/TestActivity.java
@@ -0,0 +1,15 @@
+package firstapp.studiolearn.com.firstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Bender on 27/02/2015.
+ */
+public class TestActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+    }
+}
