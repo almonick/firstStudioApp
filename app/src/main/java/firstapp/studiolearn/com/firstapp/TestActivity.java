c8d2526681710dac98b5b38b5c4ceb3ffd9ce801
almonick <almonick72@gmail.com>
2015-02-27 15:26:15 +0200

    add activity

--- app/src/main/java/firstapp/studiolearn/com/firstapp/TestActivity.java
+++ app/src/main/java/firstapp/studiolearn/com/firstapp/TestActivity.java
@@ -10,6 +10,6 @@ import android.os.PersistableBundle;
 public class TestActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.test_layout);
     }
 }
